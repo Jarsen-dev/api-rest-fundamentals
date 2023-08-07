9504f45e5116b170d5ca7097db305930afb867eb
Armando Desktop <joseaat.17@gmail.com>
2023-08-06 20:55:45 -0600

    [ADD] Axios

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+const api = axios.create({
+  baseURL: 'https://api.thedogapi.com/v1'
+});
+const AUTH_TOKEN = 'live_5gjlVeonu44CFzDm3sTaXoJ0TF8JJIow0d9Ay1GmZoeto8O8jgLZM5pJEH6KHO1z';
+api.defaults.headers.common['X-API-KEY'] = AUTH_TOKEN;
+
 const URL_API_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=2';
 const URL_API_FAVOURITES = 'https://api.thedogapi.com/v1/favourites';
 const URL_API_FAVOURITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}`;
+const URL_API_UPLOAD = 'https://api.thedogapi.com/v1/images/upload';
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const spanError = document.getElementById('error');
 const section = document.getElementById('favouriteDogs');
+const form = document.getElementById('uploadingForm');
 
 async function loadRandomDogs() {
   const response = await fetch(URL_API_RANDOM);
@@ -58,7 +66,11 @@ async function loadFavoriteDogs() {
 }
 
 async function saveFavouriteDog(id) {
-  const response = await fetch(URL_API_FAVOURITES, {
+  const { data, status } = await api.post('/favourites', {
+    image_id: id,
+  });
+
+  /*const response = await fetch(URL_API_FAVOURITES, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -69,10 +81,10 @@ async function saveFavouriteDog(id) {
     }),
   });
   
-  const data = await response.json();
+  const data = await response.json();*/
   
-  if (response.status !== 200) {
-    spanError.innerHTML = `Hubo un error: ${response.status} ${response.message}`;
+  if (status !== 200) {
+    spanError.innerHTML = `Hubo un error: ${data.status} ${data.message}`;
   } else {
     console.log('Perrito guardado en favoritos');
     loadFavoriteDogs();
@@ -94,6 +106,32 @@ async function deleteFavouriteDog(id) {
   } else {
     console.log('Perrito eliminado de favoritos');
     loadFavoriteDogs();
+  }
+}
+
+async function uploadDoggyPhoto() {
+  const formData = new FormData(form);
+
+  console.log(formData.get('file'));
+
+  const response = await fetch(URL_API_UPLOAD, {
+    method:'POST',
+    headers: {
+      //'Content-Type': 'multipart/form-data',
+      'X-API-KEY': 'live_5gjlVeonu44CFzDm3sTaXoJ0TF8JJIow0d9Ay1GmZoeto8O8jgLZM5pJEH6KHO1z',
+    },
+    body: formData,
+  })
+
+  const data = await response.json();
+
+  if (response.status !== 201) {
+    spanError.innerHTML = `Hubo un error: ${response.status} ${data.message}`
+  } else {
+    console.log("Foto de doggy cargada :)");
+    console.log({ data });
+    console.log(data.url);
+    saveFavouriteDog(data.id) //para agregar el michi cargado a favoritos.
   }
 }
 
